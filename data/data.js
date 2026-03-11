260502c3b09a4ea2ef5d0c5cbadd015947d0b88e
marvin <mcanastuj1999@gmail.com>
2026-03-11 17:54:32 -0600

    first commit

---
+++ data/data.js
@@ -0,0 +1,148 @@
+const dashboardData = {
+  header: {
+    title: "Social Media Dashboard",
+    totalFollowers: 23004
+  },
+
+  // Tarjetas principales de redes sociales
+  socialCards: [
+    {
+      platform: "facebook",
+      icon: "./images/icon-facebook.svg",
+      username: "@nathanf",
+      count: 1987,
+      label: "Followers",
+      change: {
+        value: 12,
+        isPositive: true
+      },
+      accentColor: "hsl(208, 92%, 53%)"
+    },
+    {
+      platform: "twitter",
+      icon: "./images/icon-twitter.svg",
+      username: "@nathanf",
+      count: 1044,
+      label: "Followers",
+      change: {
+        value: 99,
+        isPositive: true
+      },
+      accentColor: "hsl(203, 89%, 53%)"
+    },
+    {
+      platform: "instagram",
+      icon: "./images/icon-instagram.svg",
+      username: "@realnathanf",
+      count: "11k",
+      label: "Followers",
+      change: {
+        value: 1099,
+        isPositive: true
+      },
+      accentColor: "linear-gradient(to right, hsl(37, 97%, 70%), hsl(329, 70%, 58%))"
+    },
+    {
+      platform: "youtube",
+      icon: "./images/icon-youtube.svg",
+      username: "Nathan F.",
+      count: 8239,
+      label: "Subscribers",
+      change: {
+        value: 144,
+        isPositive: false
+      },
+      accentColor: "hsl(348, 97%, 39%)"
+    }
+  ],
+
+  // Tarjetas de resumen del día
+  overviewCards: [
+    {
+      platform: "facebook",
+      icon: "./images/icon-facebook.svg",
+      metric: "Page Views",
+      value: 87,
+      change: {
+        percentage: 3,
+        isPositive: true
+      }
+    },
+    {
+      platform: "facebook",
+      icon: "./images/icon-facebook.svg",
+      metric: "Likes",
+      value: 52,
+      change: {
+        percentage: 2,
+        isPositive: false
+      }
+    },
+    {
+      platform: "instagram",
+      icon: "./images/icon-instagram.svg",
+      metric: "Likes",
+      value: 5462,
+      change: {
+        percentage: 2257,
+        isPositive: true
+      }
+    },
+    {
+      platform: "instagram",
+      icon: "./images/icon-instagram.svg",
+      metric: "Profile Views",
+      value: "52k",
+      change: {
+        percentage: 1375,
+        isPositive: true
+      }
+    },
+    {
+      platform: "twitter",
+      icon: "./images/icon-twitter.svg",
+      metric: "Retweets",
+      value: 117,
+      change: {
+        percentage: 303,
+        isPositive: true
+      }
+    },
+    {
+      platform: "twitter",
+      icon: "./images/icon-twitter.svg",
+      metric: "Likes",
+      value: 507,
+      change: {
+        percentage: 553,
+        isPositive: true
+      }
+    },
+    {
+      platform: "youtube",
+      icon: "./images/icon-youtube.svg",
+      metric: "Likes",
+      value: 107,
+      change: {
+        percentage: 19,
+        isPositive: false
+      }
+    },
+    {
+      platform: "youtube",
+      icon: "./images/icon-youtube.svg",
+      metric: "Total Views",
+      value: 1407,
+      change: {
+        percentage: 12,
+        isPositive: false
+      }
+    }
+  ],
+
+  // Iconos para cambios positivos/negativos
+  icons: {
+    up: "./images/icon-up.svg",
+    down: "./images/icon-down.svg"
+  }
+};
---
+++ style-guide.md
@@ -0,0 +1,61 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+> 💡 These are just the design sizes. Ensure content is responsive and meets WCAG requirements by testing the full range of screen sizes from 320px to large screens.
+
+## Colors
+
+### Primary
+
+- Green 500: #1db489
+- Red 500: #dc414c
+
+- Blue 600 (Facebook): #198ff5
+- Blue 500 (Twitter): #1ca0f2
+- Linear gradient (Instagram): #fdc468 to #ee867c to #df4996
+- Red 700 (YouTube): #c4032a
+
+#### Dark Theme
+
+- Linear gradient (Toggle): #368fe7 to #3eda82
+
+#### Light Theme
+
+- Gray 400 (Toggle): #aeb3cb
+
+### Neutral
+
+#### Dark Theme
+
+- Gray 950 (BG): #1e202a
+- Gray 900 (Top BG Pattern): #1f212e
+- Navy 950 (Card BG): #252a41
+- Gray 400 (Text): #aeb3cb
+- White (Text): #ffffff
+
+#### Light Theme
+
+- White (BG): #ffffff
+- Blue 50 (Top BG Pattern): #f5f7ff
+- Navy 50 (Card BG): #f0f2fa
+- Gray 650 (Text): #63687e
+- Gray 950 (Text): #1e202a
+
+## Typography
+
+### Body Copy
+
+- Font size (Overview Card Headings): 14px
+
+### Font
+
+- Family: [Inter](https://fonts.google.com/specimen/Inter)
+- Weights: 400, 700
+
+> 💎 [Upgrade to Pro](https://www.frontendmentor.io/pro?ref=style-guide) for design file access to see all design details and get hands-on experience using a professional workflow with tools like Figma.
